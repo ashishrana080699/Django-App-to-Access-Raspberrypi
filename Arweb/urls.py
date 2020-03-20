701f3887fda133216110cf5772353ca3686e9e7b
Ashish Rana <aashishrana269@gmail.com>
2020-03-21 05:04:38 +0530

    Add files via upload

--- Arweb/urls.py
+++ Arweb/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
-    path('/on', views.on, name='on'),
-    path('/off', views.off, name='off'),
-    path('/camera', views.off, name='camera'),
+    path('on', views.on, name='on'),
+    path('off', views.off, name='off'),
+    path('camera', views.off, name='camera'),
 ]
